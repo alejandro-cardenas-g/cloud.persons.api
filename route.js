403fd5df0e4cf947f499e1e1964c9b951bef699b
alejandro-cardenas-g <alejandro.cardenas.g07@gmail.com>
2024-03-13 21:18:26 -0500

    feat: health check

--- route.js
+++ route.js
@@ -6,5 +6,10 @@ const router = Router();
 router.get("/", getPerson);
 router.post("/", createPerson);
 router.get("/search", getPerson);
+router.get("/health-check", (_, res) => {
+  return res.status(200).json({
+    status: 200,
+  });
+});
 
 export default router;
